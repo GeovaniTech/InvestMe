4211e21c0e3d0631a626e58925d9898c6d3ace0a
Geovani Debastiani <geovanidebastiani32@gmail.com>
2022-12-25 21:39:53 -0300

    atributos dashboard expenditures

--- src/main/java/bean/MBExpenditure.java
+++ src/main/java/bean/MBExpenditure.java
@@ -18,9 +18,21 @@ public class MBExpenditure implements Serializable {
 	private ManterTransaction manterTransaction;
 	private List<TOTransaction> expenditures;
 	
+	private Double spents;
+	private Double recreation;
+	private Double market;
+	private Double ifood;
+	private Double others;
+	
 	public MBExpenditure() {
 		this.manterTransaction = new ManterTransaction();
 		this.expenditures = new ArrayList<TOTransaction>();
+		this.spents = Double.valueOf(0);
+		this.recreation = Double.valueOf(0);
+		this.market = Double.valueOf(0);
+		this.ifood = Double.valueOf(0);
+		this.others = Double.valueOf(0);
+		
 		listExpendiures();
 	}
 	
@@ -42,5 +54,45 @@ public class MBExpenditure implements Serializable {
 
 	public void setExpenditures(List<TOTransaction> expenditures) {
 		this.expenditures = expenditures;
+	}
+
+	public Double getSpents() {
+		return spents;
+	}
+
+	public void setSpents(Double spents) {
+		this.spents = spents;
+	}
+
+	public Double getRecreation() {
+		return recreation;
+	}
+
+	public void setRecreation(Double recreation) {
+		this.recreation = recreation;
+	}
+
+	public Double getMarket() {
+		return market;
+	}
+
+	public void setMarket(Double market) {
+		this.market = market;
+	}
+
+	public Double getIfood() {
+		return ifood;
+	}
+
+	public void setIfood(Double ifood) {
+		this.ifood = ifood;
+	}
+
+	public Double getOthers() {
+		return others;
+	}
+
+	public void setOthers(Double others) {
+		this.others = others;
 	}
 }
