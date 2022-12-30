2f121acba46941b39422fa9c87a8a044986d9c07
Geovani Debastiani <geovanidebastiani32@gmail.com>
2022-12-29 22:39:47 -0300

    Some classes for to do the login

--- src/main/java/bean/MBUser.java
+++ src/main/java/bean/MBUser.java
@@ -1,5 +1,43 @@
 package bean;
 
-public class MBUser {
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import manter.ManterUser;
+import to.TOUser;
+
+@Named("MBUser")
+@ViewScoped
+public class MBUser implements Serializable {
+	private static final long serialVersionUID = -8527177710843576826L;
 	
+	private TOUser user;
+	private ManterUser manterUser;
+	
+	public MBUser() {
+		this.user = new TOUser();
+		this.manterUser = new ManterUser();
+	}
+
+	public void validateAccess() {
+		
+	}
+	
+	public TOUser getUser() {
+		return user;
+	}
+
+	public void setUser(TOUser user) {
+		this.user = user;
+	}
+
+	public ManterUser getManterUser() {
+		return manterUser;
+	}
+
+	public void setManterUser(ManterUser manterUser) {
+		this.manterUser = manterUser;
+	}
 }
--- src/main/java/manter/ManterUser.java
+++ src/main/java/manter/ManterUser.java
@@ -1,13 +1,63 @@
 package manter;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 
+import interfaces.GenericDAO;
+import interfaces.JPAEntity;
+import model.User;
+import to.TOUser;
+
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
-public class ManterUser {
-	public void access() {
+public class ManterUser implements GenericDAO<TOUser>, JPAEntity {
+	
+	public boolean validateAccess(TOUser toUser) {
+		User user = (User) em.createQuery(" SELECT U FROM User U WHERE U.name = :pName AND U.password = :pPassword")
+			.setParameter("pName", toUser.getName())
+			.setParameter("pPassword", toUser.getPassword())
+			.getSingleResult();
 		
+		return user != null && !user.getName().equals("");
+			
+	}
+
+	@Override
+	public void save(TOUser to) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void change(TOUser to) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(TOUser to) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TOUser findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TOUser> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TOUser> list(String specificType) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
