21253d1e715d34d101cd2ae44ebb13dbf4dc8fc3
GeovaniTech <geovanidebastiani32@gmail.com>
2024-02-10 14:38:19 -0300

    chore: Removing unused code

--- server-investme-web/src/main/webapp/resources/js/menuClient.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {  
-    if(document.getElementById("header").style.width == "0px") {
-        document.getElementById("header").style.left = "0px";
-        document.getElementById("header").style.width = "250px";
-    } else {
-        document.getElementById("header").style.left = "-300px";
-        document.getElementById("header").style.width = "0px";
-    }
-}
