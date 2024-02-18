902f764a9f3b5d3870b4dd7e564a99227576d85f
GeovaniTech <geovanidebastiani32@gmail.com>
2024-02-18 17:32:09 -0300

    fix: return production base url

--- server-investme-utils/src/main/java/security/wildfly/WildflyConfigs.java
+++ server-investme-utils/src/main/java/security/wildfly/WildflyConfigs.java
@@ -5,6 +5,11 @@ public class WildflyConfigs {
 	
 	private static String getBaseUrl() {
 		String base = System.getProperty("user.dir");
+		
+		if(base.equals("/")) {
+			return "/opt/wf-30/";
+		}
+		
 		return base.substring(0, base.length() -3);
 	}
 }
