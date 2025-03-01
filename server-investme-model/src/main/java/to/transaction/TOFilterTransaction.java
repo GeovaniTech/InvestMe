e534058adf58ab9c35f56936da8204cf61dac155
Geovani Debastiani <geovanidebastiani32@gmail.com>
2025-03-01 15:34:09 -0300

    feat: Filtering by first day of month
    Issue: https://github.com/GeovaniTech/InvestMe/issues/67

--- server-investme-model/src/main/java/to/transaction/TOFilterTransaction.java
+++ server-investme-model/src/main/java/to/transaction/TOFilterTransaction.java
@@ -1,5 +1,8 @@
 package to.transaction;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import abstracts.AbstractTOFilter;
 import to.TODateRangeFilter;
 import to.TOInputFilter;
@@ -21,6 +24,16 @@ public class TOFilterTransaction extends AbstractTOFilter implements Cloneable {
 		this.setActive(new TOInputFilter());
 		this.setDatePurchase(new TODateRangeFilter());
 		this.setPrice(new TOInputNumberFilter());
+        
+		Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        
+        this.getDatePurchase().setFrom(cal.getTime());
 	}
 	
 	public TOInputFilter getActive() {
